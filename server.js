ef1249f83f67c7992fea3562f672e1656235a9f9
oluwasamibond <oluwasamibond90@gmail.com>
2026-03-25 10:57:45 +0100

    cron-job

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ import paymentRoute from "./routes/paymentRoutes.js"
 import webhookRoutes from "./webhook/webhookRoutes.js"
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import job from "./config/cron.js";
 
 
 const port = process.env.PORT || 5000;
@@ -32,6 +33,7 @@ app.use(
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
+job.start()
 
 
 // all routes
